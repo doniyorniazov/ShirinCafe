7f52604f04109c4b2231a1bc33d42a06d3884941
doniyorniazov <niazovd@gmail.com>
2023-01-24 18:01:57 +0500

    add user model

---
+++ src/Services/ShirinCafe.API/ShirinCafe.API/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShirinCafe.API.Models
+{
+	public class User
+	{
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        [Required]
+        public string? Lastname { get; set; }
+        [Required]
+        public DateTime? BirthDate { get; set; }
+        [Required]
+        public string? Email { get; set; }       
+    }
+}
+
