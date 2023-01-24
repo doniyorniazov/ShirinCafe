2edcb25d4cb86856383aabb8c5691b8998db9e8d
doniyorniazov <niazovd@gmail.com>
2023-01-24 10:34:58 +0500

    initial welcome API

--- src/Services/ShirinCafe.API/ShirinCafe.API/Program.cs
+++ src/Services/ShirinCafe.API/ShirinCafe.API/Program.cs
@@ -16,27 +16,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/", () => "Welcome to Shirin Cafe API");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
-app.Run();
+app.Run("http://localhost:2222");
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
